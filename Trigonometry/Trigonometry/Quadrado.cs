ad61d7f0bad20b72da3c5bc28f9653697dc82643
JuanMa Garcia <juanmgar@gmail.com>
2024-04-10 15:49:21 +0100

    Se crean las clases Cuadrado y Rectángulo

---
+++ Trigonometry/Trigonometry/Quadrado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Quadrado
+{
+	private double _lado = 1;
+
+    public double Lado {
+		get { return _lado; }
+		set {
+			if (value > 0)
+				_lado = value;
+		}
+	}
+    public Quadrado(){} //  Constructor vacio
+
+	public Quadrado(double l): base() { Lado = l; } // Constructor por parámetros. Con base te aseguras que si el valor es negativo al menos te construirá con valor 1.
+
+	public Quadrado(Quadrado q) { Lado = q.Lado; } // Constructor copia
+
+	public double Perimeter()
+	{
+		return Lado + Lado + Lado + Lado;
+	}
+
+	public double Area()
+	{
+		return Lado * Lado;
+	}
+
+	public double Diagonal()
+	{
+		return Math.Sqrt(2 * Math.Pow(Lado, 2));
+
+	}
+
+}
---
+++ Trigonometry/Trigonometry/Rectangulo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Rectangulo
+{
+
+	private double _base = 1;
+
+	public double Base
+	{
+		get { return _base; }
+		set
+		{
+			if (value > 0)
+				_base = value;
+		}
+	}
+
+	private double _altura = 1;
+
+	public double Altura
+	{
+		get { return _altura; }
+		set
+		{
+			if (value > 0)
+				_altura = value;
+		}
+	}
+
+	public Rectangulo() { } //  Constructor vacio
+
+	public Rectangulo(double b, double a) : base() { Base = b; Altura = a; } // Constructor por parámetros. Con base te aseguras que si el valor es negativo al menos te construirá con valor 1.
+
+	public Rectangulo(Rectangulo r) { Base = r.Base; Altura = r.Altura; } // Constructor copia
+
+	public double Perimeter()
+	{
+		return (Base + Altura) * 2;
+	}
+
+	public double Area()
+	{
+		return Base * Altura;
+	}
+}
